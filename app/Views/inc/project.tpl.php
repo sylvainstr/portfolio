2efda6c8c97eb7fc40468da833a69fe81fd52c04
sylvainstr <sylvain.sainteromane@gmail.com>
2022-06-03 12:29:14 +0200

    add integration card formation

--- app/Views/inc/project.tpl.php
+++ app/Views/inc/project.tpl.php
@@ -1,3 +1,32 @@
 <section class="projects" id="projects">
+  <h1 class="projects-title">Réalisations</h1>
+  <div class="line-sm"></div>
 
-      </section>
+  <div class="projects-group">
+    <div class="my-projects">
+      <div class="my-projects-img-security">
+        <img class="security-response" src="<?= $absoluteUrl ?>assets/images/security-concept.png" alt="image du site security respone">
+        <h3><a href="https://www.security-response.fr/" target="_blank">Voir le site</a></h3>
+      </div>
+
+      <div class="my-projects-text">
+        <h1>Security Response</h1>
+
+      </div>
+    </div>
+
+    <div class="my-projects">
+      
+      <div class="my-projects-text">
+        <h1>Ocoeur2soi</h1>
+        
+      </div>
+      <div class="my-projects-img-ocoeur">
+        <img class="ocoeur" src="<?= $absoluteUrl ?>assets/images/reflex.png" alt="image du site ocoeur2soi">
+        <h3><a href="https://www.ocoeur2soi.fr/" target="_blank">Voir le site</a></h3>
+      </div>
+    </div>
+
+  </div>
+
+</section>
