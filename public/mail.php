981a22ef7d68746548f49180691fd5c2e98b3997
sylvainstr <sylvain.sainteromane@gmail.com>
2022-06-14 19:20:31 +0200

    fix bug redirection

--- public/mail.php
+++ public/mail.php
@@ -44,8 +44,8 @@ require $_SERVER['DOCUMENT_ROOT'] . '/../vendor/phpmailer/phpmailer/src/SMTP.php
       $mail->AltBody = $message;
 
         $mail->send();
-        // echo "Bonjour <b> $name </b>, votre message a bien été envoyé !!";
         header("Location: https://www.sylvainsainteromane.com/");        
+        echo "Bonjour <b> $name </b>, votre message a bien été envoyé !!";
         
       } catch (Exception $e) {
         echo "Message non envoyé. Erreur: {$mail->ErrorInfo}";
