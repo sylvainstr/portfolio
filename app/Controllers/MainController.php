621bb18790bed4ede80b37a06f1bb7f66cbe71ce
sylvainstr <sylvain.sainteromane@gmail.com>
2022-06-14 14:24:29 +0200

    update smtp

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -20,34 +20,34 @@ class MainController extends CoreController
 
   public function contact()
   {
-    $mail = new PHPMailer(true);
+      $mail = new PHPMailer(true);
 
-    try {
+      try {
         //Server settings
         $mail->SMTPDebug = SMTP::DEBUG_SERVER;                   //Enable verbose debug output
         $mail->isSMTP();                                        //Send using SMTP
-        $mail->Host       = 'smtp://sylvainsainteromane.com/';                 //Set the SMTP server to send through
+        $mail->Host       = 'bij.o2switch.net';                 //Set the SMTP server to send through
         $mail->Port       = 26;                               //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
         $mail->CharSet = 'utf-8';                                  //Charset
-
+    
         //Recipients
-        $mail->setFrom('no-reply@sylvain.sainteromane.fr');
-        $mail->addAddress('contact@sylvain.sainteromane.fr');     //Add a recipient
+        $mail->setFrom('no-reply@sylvainsainteromane.com');
+        $mail->addAddress('contact@sylvainsainteromane.com');     //Add a recipient
         // $mail->addAddress('ellen@example.com');               //Name is optional
         // $mail->addReplyTo('info@example.com', 'Information');
         // $mail->addCC('cc@example.com');
         // $mail->addBCC('bcc@example.com');
-
+    
         //Attachments
         // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
         // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-
+    
         //Content
         $mail->isHTML(true);                                  //Set email format to HTML
         $mail->Subject = 'Sujet du message';
         $mail->Body    = 'Message';
         $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
+    
         $mail->send();
         echo 'Le message a été envoyé';
     } catch (Exception $e) {
