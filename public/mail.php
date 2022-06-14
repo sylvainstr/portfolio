2844022fd8bbc194ed54783b9746ff210ae585d5
sylvainstr <sylvain.sainteromane@gmail.com>
2022-06-14 16:27:12 +0200

    fix bug redirection

--- public/mail.php
+++ public/mail.php
@@ -46,11 +46,12 @@ require $_SERVER['DOCUMENT_ROOT'] . '/../vendor/phpmailer/phpmailer/src/SMTP.php
         $mail->send();
         echo "Bonjour <b> $name </b>, votre message a bien été envoyé !!";
 
-        header("Location: https:$absoluteUrl");        
-        exit;
-
-    } catch (Exception $e) {
+        
+      } catch (Exception $e) {
         echo "Message non envoyé. Erreur: {$mail->ErrorInfo}";
-    }
+      }
+      
+      header("Location: https:$absoluteUrl");        
+      exit;
       
 
