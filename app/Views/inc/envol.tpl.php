24e71669806081770fafb4b698024f100eeb4c37
sylvainstr <sylvain.sainteromane@gmail.com>
2022-12-08 12:21:20 +0100

    integration envol page DONE

--- app/Views/inc/envol.tpl.php
+++ app/Views/inc/envol.tpl.php
@@ -13,6 +13,216 @@
   <div class="envol-who">
     <h1>L’ENVOL, c’est qui ?</h1>
     <div class="line-large"></div>
+
+    <!-- profil Gaelle -->
+    <div class="envol-who-card">
+      <div class="who-card-img">
+        <img src="<?= $absoluteUrl ?>assets/images/gaelle.png" alt="image de profil">
+      </div>
+      <div class="card-group">
+        <div class="who-card-title">
+          <h2>Gaëlle JOSEPH – Caméléonienne – Rédactrice Web</h2>
+        </div>
+        <div class="social">
+          <a class="facebook" href="https://www.facebook.com/cameleonienne" target="_blank">
+            <img src="<?= $absoluteUrl ?>assets/images/facebook2.png" alt="logo de facebook">
+          </a>
+          <a href="https://www.linkedin.com/in/gaelle-joseph/" target="_blank">
+            <img src="<?= $absoluteUrl ?>assets/images/linkedin2.png" alt="logo de linkedin">
+          </a>
+        </div>
+        <div class="card-description">
+          <p>
+            C’est depuis l’île de la Réunion que j’adore écouter les femmes et les hommes entrepreneur(e)s me raconter leur parcours, leur passion, leur vision… Puis, jouer avec les mots, les manier, les ordonner, pour en extraire l’essence : une vision, une singularité, une authenticité, une signature.
+          </p>
+          <p>
+            En plus de savoir écrire pour les humains, je sais aussi écrire pour les robots.
+            Si si ! C’est ce qu’on appelle la rédaction web SEO : écrire aussi pour que Google trouve, comprenne et approuve les contenus.
+          </p>
+          <p>
+            Voici comment je peux vous accompagner dans votre projet, depuis l’élaboration de votre stratégie digitale jusqu’à la gestion de vos réseaux sociaux, en passant par la stratégie de contenus, la rédaction d’articles de blog ou encore l’audit SEO de votre site.
+          </p>
+        </div>
+
+        <div class="card-quality-group">
+          <div class="card-quality">
+            <img src="<?= $absoluteUrl ?>assets/images/etoile.png" alt="image d'étoile">
+            <p>
+              stratégie digitale
+            </p>
+          </div>
+          <div class="card-quality">
+            <img src="<?= $absoluteUrl ?>assets/images/etoile.png" alt="image d'étoile">
+            <p>
+              audit seo
+            </p>
+          </div>
+          <div class="card-quality">
+            <img src="<?= $absoluteUrl ?>assets/images/etoile.png" alt="image d'étoile">
+            <p>
+              rédaction web seo
+            </p>
+          </div>
+          <div class="card-quality">
+            <img src="<?= $absoluteUrl ?>assets/images/etoile.png" alt="image d'étoile">
+            <p>
+              réseaux sociaux
+            </p>
+          </div>
+          <div class="card-quality">
+            <img src="<?= $absoluteUrl ?>assets/images/etoile.png" alt="image d'étoile">
+            <p>
+              optimisation seo
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <!-- profil Mathieu -->
+
+    <div class="envol-who-card">
+      <div class="card-group">
+        <div class="who-card-title">
+          <h2>Mathieu ADER – Studio M42 – Webdesigner Graphiste</h2>
+        </div>
+        <div class="social">
+          <a class="facebook" href="https://www.facebook.com/M42StudioGraph" target="_blank">
+            <img src="<?= $absoluteUrl ?>assets/images/facebook2.png" alt="logo de facebook">
+          </a>
+          <a href="https://www.linkedin.com/company/studio-graphique-m42/" target="_blank">
+            <img src="<?= $absoluteUrl ?>assets/images/linkedin2.png" alt="logo de linkedin">
+          </a>
+        </div>
+        <div class="card-description">
+          <p>
+            Installé à Barjols, en Provence Verte, je crée des identités visuelles et des sites Web sur WordPress. Des sites uniques et élégants, pour présenter tes activités et leur donner une belle visibilité.
+          </p>
+          <p>
+            J’aime découvrir de nouveaux projets, de nouvelles activités. Être à l’écoute de toutes tes idées et leur donner vie à travers un design graphique qui te représente et qui plaira à tes clients, même dans 10 ans.
+          </p>
+          <p>
+            Ma priorité : m’assurer que tu vis une excellente expérience, avant, pendant et après la mise en ligne de ton site.
+          </p>
+          <p>
+            C’est pourquoi je t’accompagne à toutes les étapes de ton projet, depuis le choix de l’hébergeur et du nom de domaine jusqu’à la mise en conformité RGPD :
+          </p>
+        </div>
+
+        <div class="card-quality-group">
+          <div class="card-quality">
+            <img src="<?= $absoluteUrl ?>assets/images/etoile.png" alt="image d'étoile">
+            <p>
+              création graphique
+            </p>
+          </div>
+          <div class="card-quality">
+            <img src="<?= $absoluteUrl ?>assets/images/etoile.png" alt="image d'étoile">
+            <p>
+              identité visuelle
+            </p>
+          </div>
+          <div class="card-quality">
+            <img src="<?= $absoluteUrl ?>assets/images/etoile.png" alt="image d'étoile">
+            <p>
+              site vitrine et e-commerce
+            </p>
+          </div>
+          <div class="card-quality">
+            <img src="<?= $absoluteUrl ?>assets/images/etoile.png" alt="image d'étoile">
+            <p>
+              formation wordpress
+            </p>
+          </div>
+          <div class="card-quality">
+            <img src="<?= $absoluteUrl ?>assets/images/etoile.png" alt="image d'étoile">
+            <p>
+              maintenance site web
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="who-card-img-right">
+        <img src="<?= $absoluteUrl ?>assets/images/mathieu.png" alt="image de profil">
+      </div>
+    </div>
+
+    <!-- profil Sylvain -->
+    <div class="envol-who-card">
+      <div class="who-card-img">
+        <img src="<?= $absoluteUrl ?>assets/images/sylvain.png" alt="image de profil">
+      </div>
+      <div class="card-group">
+        <div class="who-card-title">
+          <h2>Sylvain SAINTE ROMANE – STR Web – Développeur Web</h2>
+        </div>
+        <div class="social">
+          <a href="https://www.linkedin.com/in/sylvain-sainte-romane/" target="_blank">
+            <img src="<?= $absoluteUrl ?>assets/images/linkedin2.png" alt="logo de linkedin">
+          </a>
+        </div>
+        <div class="card-description">
+          <p>
+            C’est depuis la région toulousaine que j'ai créé une entreprise de création de sites internet dans laquelle je réalise des prestations sur mesure.
+            Je m'épanouis pleinement dans mon travail, j'apprends de nouvelles notions chaque jour, j'aime le travail en équipe et j'ai besoin de challenges.
+
+          </p>
+          <p>
+            Depuis 2021,<br>
+            Je donne vie à vos idées en réalisant des projets sur mesure;
+            des prestations adaptées à vos besoins.
+
+          </p>
+          <p>
+            Mon objectif est de réaliser le projet qui répondra aux attentes de mes clients.
+            Je réalise un site personnalisé, unique et qualitatif.
+
+          </p>
+          <p>
+            Je vous propose un accompagnement tout le long du projet, je suis réactif et je reste à l’écoute des mes clients.
+          </p>
+          <p>
+            « Les clients ne peuvent pas toujours vous dire ce qu'ils veulent, mais ils sont toujours capables d'exprimer ce qui ne va pas. »
+          </p>
+        </div>
+
+        <div class="card-quality-group">
+          <div class="card-quality">
+            <img src="<?= $absoluteUrl ?>assets/images/etoile.png" alt="image d'étoile">
+            <p>
+              création de sites
+            </p>
+          </div>
+          <div class="card-quality">
+            <img src="<?= $absoluteUrl ?>assets/images/etoile.png" alt="image d'étoile">
+            <p>
+              prestations sur mesure
+            </p>
+          </div>
+          <div class="card-quality">
+            <img src="<?= $absoluteUrl ?>assets/images/etoile.png" alt="image d'étoile">
+            <p>
+              responsive design
+            </p>
+          </div>
+          <div class="card-quality">
+            <img src="<?= $absoluteUrl ?>assets/images/etoile.png" alt="image d'étoile">
+            <p>
+              interface d'administration
+            </p>
+          </div>
+          <div class="card-quality">
+            <img src="<?= $absoluteUrl ?>assets/images/etoile.png" alt="image d'étoile">
+            <p>
+              un site sécurisé
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
   </div>
 
   <div class="envol-how">
